1c1d0c291717edc550172cf8f7a325f5d43f5dd9
Suein Choi <48196612+sueinchoi@users.noreply.github.com>
2023-11-21 23:39:32 +0900

    Update Kpcoeff.R

--- R/Kpcoeff.R
+++ R/Kpcoeff.R
@@ -1,16 +1,12 @@
 
-pcoeffs <- function(logP, pKa, fup, BP, type = 3, pred = "P&T", dat) {
+KpCoeff <- function(logP, pKa, fup, BP, type = 3, pred = "P&T", dat) {
   if (pred == "P&T") {
-    source("Kp_calculator/code/CalcKp_P&T.R")
     pcoeff <- calcKp_PT(logP = logP, pKa = pKa, fup = fup, BP = BP, type = type)
-  } else if (pred == "Berez") { # Berezhkovskiy
-    source("Kp_calculator/code/CalcKp_Berez.R")
+  } else if (pred == "Berez") { 
     pcoeff <- calcKp_Berez(logP = logP, pKa = pKa, fup = fup, BP = BP, type = type)
-  } else if (pred == "Schmitt") { # Schmitt, Walter 2008
-    source("Kp_calculator/code/CalcKp_Schmitt.R")
+  } else if (pred == "Schmitt") { 
     pcoeff <- calcKp_Schmitt(logP = logP, pKa = pKa, fup = fup, type = type)
   } else {
-    source("Kp_calculator/code/CalcKp_R&R.R") # Rodgers and Rowland 2006
     pcoeff <- calcKp_RR(logP = logP, pKa = pKa, fup = fup, BP = BP, type = type)
   }
   
