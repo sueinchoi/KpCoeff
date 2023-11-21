521a22098c6716444a91126282d2741efe29e72b
Suein Choi <48196612+sueinchoi@users.noreply.github.com>
2023-11-21 23:49:19 +0900

    update

--- R/Kpcoeff.R
+++ R/Kpcoeff.R
@@ -1,5 +1,5 @@
 
-KpCoeff <- function(logP, pKa, fup, BP, type = 3, pred = "P&T", dat) {
+Kpcoeff <- function(logP, pKa, fup, BP, type = 3, pred = "P&T", dat) {
   if (pred == "P&T") {
     pcoeff <- Kpcoeff_PT(logP = logP, pKa = pKa, fup = fup, BP = BP, type = type)
   } else if (pred == "Berez") { 
